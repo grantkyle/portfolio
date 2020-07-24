92d49b89910e07356a942f847706429d40aba815
Grant Kyle <sgrantkyle@gmail.com>
2020-07-24 17:58:56 -0500

    svg logos added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
     <Router>
           <div className='App'>
           <Route exact path='/' component={Home} />
+          {/* <Route exact path='/projects' component={Projects} />
+          <Route exact path='/blog' component={Blog} /> */}
           </div>
         </Router>
     
